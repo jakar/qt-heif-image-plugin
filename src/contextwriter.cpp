34363d16a61767648bfc0e6ba760feefac24ea6f
Jake Harr <jakeharr@gmail.com>
2018-07-16 15:10:49 -0400

    contextwriter: Remove unnecessary constants
    Each error is only used in one place.

--- src/contextwriter.cpp
+++ src/contextwriter.cpp
@@ -4,22 +4,6 @@
 
 #include <cstdint>
 
-namespace {
-
-constexpr heif_error kOkError{
-    heif_error_Ok,
-    heif_suberror_Unspecified,
-    "ok"
-};
-
-constexpr heif_error kWriteError{
-    heif_error_Encoding_error,
-    heif_suberror_Cannot_write_output_data,
-    "write failed"
-};
-
-}  // namespace
-
 namespace qtheifimageplugin {
 
 ContextWriter::ContextWriter(QIODevice& device) :
@@ -35,10 +19,18 @@ heif_error ContextWriter::write(const void* data, size_t size)
         log::warning() << "write failed: "
             << bytesWritten << " / " << size << " bytes written";
 
-        return kWriteError;
+        return {
+            heif_error_Encoding_error,
+            heif_suberror_Cannot_write_output_data,
+            "write failed"
+        };
     }
 
-    return kOkError;
+    return {
+        heif_error_Ok,
+        heif_suberror_Unspecified,
+        "ok"
+    };
 }
 
 }  // namespace qtheifimageplugin
